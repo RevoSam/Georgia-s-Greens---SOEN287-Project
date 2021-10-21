0fd6842276c107049a325f27c54750dcd4d5ca1b
RevoSam <n-sami-m@hotmail.fr>
2021-10-20 20:42:12 -0400

    ShowMore Button Paper Towel Page

--- scripts/script.js
+++ scripts/script.js
@@ -13,11 +13,31 @@ function changeQty(e) {
     textfield.value = fieldValue;
   } else {
     fieldValue = fieldValue - 1;
-    if (fieldValue < 0) fieldValue = 0;
+    if (fieldValue <= 0) fieldValue = 1;
     textfield.value = fieldValue;
   }
 }
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", changeQty);
+}
+
+let showMoreBtn = document.getElementsByClassName("show-more");
+
+function showMoreDescription(e) {
+  e.preventDefault();
+  let showContent = document.getElementById("content-description");
+  let linkText = this.innerHTML.toUpperCase();
+  if (linkText === "SHOW MORE") {
+    linkText = "Show Less";
+    showContent.setAttribute("class", "showContent");
+  } else {
+    linkText = "Show More";
+    showContent.setAttribute("class", "hideContent");
+  }
+  this.innerHTML = linkText;
+}
+
+for (var i = 0; i < showMoreBtn.length; i++) {
+  showMoreBtn[i].addEventListener("click", showMoreDescription);
 }
