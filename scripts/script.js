bf3974257bd8848f0c6e802649803cff77214c82
RevoSam <n-sami-m@hotmail.fr>
2021-10-20 21:30:49 -0400

    Add to Cart Test-Paper Towel

--- scripts/script.js
+++ scripts/script.js
@@ -41,3 +41,31 @@ function showMoreDescription(e) {
 for (var i = 0; i < showMoreBtn.length; i++) {
   showMoreBtn[i].addEventListener("click", showMoreDescription);
 }
+
+let addCart = document.getElementById("add2Cart");
+
+function Product(id, item, price, qty) {
+  this.id = id;
+  this.item = item;
+  this.price = parseFloat(price.replace("$", ""));
+  this.qty = parseInt(qty);
+}
+
+function add_to_cart(e) {
+  e.preventDefault();
+  let id = document.getElementsByClassName("item_number")[0].innerHTML;
+  let item = document.getElementsByClassName("item_description")[0].innerHTML;
+  let price = document.getElementsByClassName("item_price")[0].innerHTML;
+  let qty = document.getElementById("quantity-text").value;
+  let product = new Product(id, item, price, qty);
+  var jsonString = JSON.stringify(product);
+  sessionStorage.setItem("cart", jsonString);
+  get_cartStorage();
+}
+
+addCart.addEventListener("click", add_to_cart);
+
+function get_cartStorage() {
+  let cartObject = JSON.parse(sessionStorage.getItem("cart"));
+  console.log(cartObject);
+}
