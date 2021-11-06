cc969eb4630b096ed772b33c3cab90d0cc2de04f
akleidotos <akleidotos@protonmail.com>
2021-11-06 18:37:45 -0400

    added cart dynamic buttons and input boxes

--- scripts/script.js
+++ scripts/script.js
@@ -34,6 +34,107 @@ for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", changeQty);
 }
 
+
+/*update price using cart buttons */
+
+let cartButtons = document.getElementsByClassName("cart-item-quantity-button");
+
+function changeCartQty(e) {
+
+  var myString = this.className.split(" ")[1].match(/item\d+/);
+  var int_data = parseInt(document.getElementById(myString).value,10);
+
+  if(this.value == "+")
+  {
+    if(document.getElementById(myString).value == "")
+      document.getElementById(myString).value = 1;
+    else
+      document.getElementById(myString).value = int_data + 1;
+      getCartTotal(myString,this.value);
+  }
+  else
+  {
+    if(document.getElementById(myString).value == "")
+      document.getElementById(myString).value = 0;
+
+    else if(!checkOOB(int_data-1))
+    {
+      document.getElementById(myString).value = int_data - 1;
+      getCartTotal(myString,this.value);
+    }
+  }
+}
+
+function checkOOB(value) {
+  if(value < 0)
+      return true;
+}
+
+for (var i = 0; i < cartButtons.length; i++) {
+  cartButtons[i].addEventListener("click", changeCartQty);
+}
+
+function getCartTotal(myString,sign)
+{
+  var new_subtotal = parseFloat(document.getElementById("subtotal").innerHTML);
+  
+  if(sign == "+")
+      new_subtotal = new_subtotal + parseFloat(document.getElementById(myString+"_price").innerHTML);
+  else
+      new_subtotal = new_subtotal - parseFloat(document.getElementById(myString+"_price").innerHTML);
+  
+  var GST = new_subtotal * 0.05;
+  var QST = new_subtotal * 0.09975;
+  
+  document.getElementById("subtotal").innerHTML = parseFloat(new_subtotal).toFixed(2);
+  document.getElementById("gst_total").innerHTML = parseFloat(GST).toFixed(2);
+  document.getElementById("qst_total").innerHTML = parseFloat(QST).toFixed(2);
+  document.getElementById("total").innerHTML = parseFloat(parseFloat(document.getElementById("subtotal").innerHTML) + parseFloat(document.getElementById("gst_total").innerHTML) + parseFloat(document.getElementById("qst_total").innerHTML)).toFixed(2);
+}
+
+/*End of update price using cart buttons*/
+
+
+//Update price using the input box
+
+var inputBoxes = document.getElementsByClassName("cart-item-quantity-input");
+
+function changeCartBox() {
+  if(isNaN(this.value))
+  {
+    alert("Only numbers are allowed")
+    this.value = 0;
+  }
+  else if(this.value.indexOf(".")!= -1)
+  {
+    alert("Cannot use decimal values");
+    this.value = parseInt(this.value);
+  }
+  var total = 0;
+  for(var i = 0; i < inputBoxes.length; i++)
+    total += inputBoxes[i].value * parseFloat(document.getElementById("item" + (i+1) + "_price").innerHTML);
+  document.getElementById("subtotal").innerHTML = parseFloat(total).toFixed(2);
+  document.getElementById("gst_total").innerHTML = parseFloat(total*0.05).toFixed(2);
+  document.getElementById("qst_total").innerHTML = parseFloat(total*0.0975).toFixed(2);
+  document.getElementById("total").innerHTML = parseFloat(parseFloat(document.getElementById("subtotal").innerHTML) + parseFloat(document.getElementById("gst_total").innerHTML) + parseFloat(document.getElementById("qst_total").innerHTML)).toFixed(2);
+}
+
+for(var i = 0; i < inputBoxes.length; i++)
+{
+  inputBoxes[i].addEventListener("keyup",changeCartBox)
+}
+
+//end of price update using input box
+
+
+
+
+
+
+
+
+
+
 //The function loads the appropriate values for the specific page from sessionStorage. Called when page is first loaded. 
 function getQty()
 {
@@ -44,7 +145,6 @@ function getQty()
 }
 
 window.onload = getQty(); //On page load, call getQty
-
 //Delete all stored quantities and values of this page from sessionStorage
 /*window.onbeforeunload = function(){
   //deleteStorage();
@@ -82,6 +182,7 @@ for (var i = 0; i < showMoreBtn.length; i++) {
   showMoreBtn[i].addEventListener("click", showMoreDescription);
 }
 
+
 let addCart = document.getElementById("add2Cart");
 
 function Product(id, item, price, qty) {
@@ -90,6 +191,7 @@ function Product(id, item, price, qty) {
   this.price = parseFloat(price.replace("$", ""));
   this.qty = parseInt(qty);
 }
+
 
 function add_to_cart(e) {
   e.preventDefault();
@@ -110,7 +212,3 @@ function get_cartStorage() {
   console.log(cartObject);
 
 }
-
-
-
-
