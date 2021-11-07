4b7a5ee9019d214caee3d35c69c802db049ce984
akleidotos <akleidotos@protonmail.com>
2021-11-07 15:45:40 -0500

    removed last changes

--- scripts/script.js
+++ scripts/script.js
@@ -391,7 +391,7 @@ function deleteEntireRow(e) {
 function updateExtendedTtl() {
   document.getElementById("total").value =
     (
-      parseFloat(document.getElementById("priceNum").innerText) *
+      parseFloat(document.getElementById("price").innerText) *
       document.getElementById("quantity-text").value
     ).toFixed(2) + "$";
 }
